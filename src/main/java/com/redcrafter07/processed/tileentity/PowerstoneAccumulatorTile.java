3ad24b687b87f16282c485829e6b77d809ee5284
RedCrafter07 <github@redcrafter07.de>
2022-04-08 16:16:20 +0200

    Powerstone accumulator tile

---
+++ src/main/java/com/redcrafter07/processed/tileentity/PowerstoneAccumulatorTile.java
@@ -0,0 +1,89 @@
+package com.redcrafter07.processed.tileentity;
+
+import com.redcrafter07.processed.blocks.PowerstoneAccumulator;
+import com.redcrafter07.processed.data.recipes.CraftingStationRecipe;
+import com.redcrafter07.processed.data.recipes.ModRecipeTypes;
+import com.redcrafter07.processed.item.ModItems;
+import net.minecraft.block.BlockState;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class PowerstoneAccumulatorTile extends TileEntity {
+    private final ItemStackHandler itemHandler = createHandler();
+    private final LazyOptional<IItemHandler> handler = LazyOptional.of(() -> itemHandler);
+
+    public PowerstoneAccumulatorTile(TileEntityType<?> tileEntityType) {
+        super(tileEntityType);
+    }
+
+    public PowerstoneAccumulatorTile()    {
+        this(ModTileEntities.POWERSTONE_ACCUMULATOR_TILE.get());
+    }
+
+    @Override
+    public void read(BlockState blockState, CompoundNBT nbt) {
+        itemHandler.deserializeNBT(nbt.getCompound("powerstoneAccumulatorContents"));
+        super.read(blockState, nbt);
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT nbt) {
+        nbt.put("powerstoneAccumulatorContents", itemHandler.serializeNBT());
+        return super.write(nbt);
+    }
+
+    private ItemStackHandler createHandler()    {
+        return new ItemStackHandler(1) {
+            @Override
+            protected void onContentsChanged(int slot) {
+                markDirty();
+            }
+
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                switch (slot)   {
+                    case 0:
+                        return stack.getItem() == ModItems.OVERLOAD_BATTERY.get();
+                    default: return true;
+                }
+            }
+
+            @Override
+            public int getSlotLimit(int slot) {
+                return 1;
+            }
+
+            @Nonnull
+            @Override
+            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+                if(!isItemValid(slot, stack))   {
+                    return stack;
+                }
+
+                return super.insertItem(slot, stack, simulate);
+            }
+        };
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)    {
+            return handler.cast();
+        }
+        return super.getCapability(cap, side);
+    }
+}
