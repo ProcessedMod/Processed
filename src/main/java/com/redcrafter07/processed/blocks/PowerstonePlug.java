242e97f8ce677c87ddbf76fd2a4d0c0561f9dae4
RedCrafter07 <github@redcrafter07.de>
2022-04-08 16:15:37 +0200

    Powerstone Plug blockstate and updating

--- src/main/java/com/redcrafter07/processed/blocks/PowerstonePlug.java
+++ src/main/java/com/redcrafter07/processed/blocks/PowerstonePlug.java
@@ -1,7 +1,11 @@
 package com.redcrafter07.processed.blocks;
 
+import com.redcrafter07.processed.tileentity.ModTileEntities;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -9,11 +13,15 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
+import javax.annotation.Nullable;
 import java.util.stream.Stream;
 
 public class PowerstonePlug extends Block {
+    public static final BooleanProperty POWERED = BooleanProperty.create("powered");
+
     public PowerstonePlug(Properties properties) {
         super(properties);
+        this.setDefaultState(this.getDefaultState().with(POWERED, false));
     }
 
     public static final VoxelShape SHAPE = Stream.of(
@@ -33,5 +41,21 @@ public class PowerstonePlug extends Block {
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return ModTileEntities.POWERSTONE_PLUG_TILE.get().create();
     }
 }
