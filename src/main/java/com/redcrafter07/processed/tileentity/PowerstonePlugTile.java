27e67e2c5751825a8a66836d35778aac41d413d8
RedCrafter07 <github@redcrafter07.de>
2022-04-08 17:52:36 +0200

    Powerstone Plug subtracts accumulator fill state

--- src/main/java/com/redcrafter07/processed/tileentity/PowerstonePlugTile.java
+++ src/main/java/com/redcrafter07/processed/tileentity/PowerstonePlugTile.java
@@ -1,6 +1,7 @@
 package com.redcrafter07.processed.tileentity;
 
 import com.redcrafter07.processed.blocks.ModBlocks;
+import com.redcrafter07.processed.blocks.PowerstoneAccumulator;
 import com.redcrafter07.processed.blocks.PowerstonePlug;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -18,20 +19,23 @@ public class PowerstonePlugTile extends TileEntity implements ITickableTileEntit
         super(tileEntityType);
     }
 
-    public PowerstonePlugTile()    {
+    public PowerstonePlugTile() {
         this(ModTileEntities.POWERSTONE_PLUG_TILE.get());
     }
 
     public boolean checkAccumulator(int x, int y, int z, World worldIn) {
-        y-=1;
+        y -= 1;
 
-        return worldIn.getBlockState(new BlockPos(x,y,z)).getBlock() == ModBlocks.POWERSTONE_ACCUMULATOR.get().getBlock();
+        return worldIn.getBlockState(new BlockPos(x, y, z)).getBlock() == ModBlocks.POWERSTONE_ACCUMULATOR.get().getBlock() && worldIn.getBlockState(new BlockPos(x, y, z)).get(PowerstoneAccumulator.FILL_STATE) > 0;
     }
 
     public void updateBlockState(BlockPos pos, World worldIn, BlockState state) {
         boolean powered = checkAccumulator(pos.getX(), pos.getY(), pos.getZ(), worldIn);
-        if (powered) {
+        BlockPos accumulatorPos = new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ());
+        BlockState accumulatorState = worldIn.getBlockState(accumulatorPos);
+        if (powered && !worldIn.isBlockPowered(pos)) {
             worldIn.setBlockState(pos, state.with(POWERED, Boolean.TRUE));
+            worldIn.setBlockState(accumulatorPos, accumulatorState.with(PowerstoneAccumulator.FILL_STATE, accumulatorState.get(PowerstoneAccumulator.FILL_STATE) - 1), 3);
         } else {
             worldIn.setBlockState(pos, state.with(POWERED, Boolean.FALSE));
         }
