af9ec4083e81e8b3a9fe6780446599de51eda4e0
RedCrafter07 <github@redcrafter07.de>
2022-04-10 22:49:28 +0200

    Add Powerstone Receiver Tile

---
+++ src/main/java/com/redcrafter07/processed/tileentity/PowerstoneReceiverTile.java
@@ -0,0 +1,69 @@
+package com.redcrafter07.processed.tileentity;
+
+import com.redcrafter07.processed.blocks.ModBlocks;
+import com.redcrafter07.processed.blocks.PowerstonePlugBlock;
+import com.redcrafter07.processed.blocks.PowerstoneReceiverBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class PowerstoneReceiverTile extends TileEntity implements ITickableTileEntity {
+    public static final BooleanProperty PLUGGED = PowerstoneReceiverBlock.PLUGGED;
+
+    public PowerstoneReceiverTile(TileEntityType<?> tileEntityTypeIn) {
+        super(tileEntityTypeIn);
+    }
+
+    public PowerstoneReceiverTile() {
+        this(ModTileEntities.POWERSTONE_RECEIVER_TILE.get());
+    }
+
+    public boolean checkPlug(int x, int y, int z, int radius, World worldIn) {
+        x -= radius;
+        y -= radius;
+        z -= radius;
+
+        int u = x;
+        int v = y;
+        int w = z;
+
+        for (int k = 0; k < (radius * 2 + 1); k++) {
+            x = u;
+            for (int j = 0; j < (radius * 2 + 1); j++) {
+                z = w;
+                for (int i = 0; i < (radius * 2 + 1); i++) {
+                    Block block = worldIn.getBlockState(new BlockPos(x, y, z)).getBlock();
+
+                    if (block == ModBlocks.POWERSTONE_PLUG.get().getBlock() && worldIn.getBlockState(new BlockPos(x, y, z)).get(PowerstonePlugBlock.POWERED)) {
+                        return true;
+                    }
+                    z++;
+                }
+                x++;
+            }
+            y++;
+        }
+        return false;
+    }
+
+    public void updateBlockState(BlockPos pos, World worldIn, BlockState state) {
+        boolean plugged = checkPlug(pos.getX(), pos.getY(), pos.getZ(), 15, worldIn);
+        if (plugged) {
+            worldIn.setBlockState(pos, state.with(PLUGGED, Boolean.TRUE));
+        } else {
+            worldIn.setBlockState(pos, state.with(PLUGGED, Boolean.FALSE));
+        }
+    }
+
+    @Override
+    public void tick() {
+        if (world.isRemote()) return;
+
+        updateBlockState(pos, world, getBlockState());
+    }
+}
