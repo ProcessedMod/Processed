c27dd45a992028858b0bd6cc33c7ce9d3ef2ee8b
RedCrafter07 <github@redcrafter07.de>
2022-04-11 20:39:55 +0200

    Create AdvancedLightningConcentratorRecipe type

---
+++ src/main/java/com/redcrafter07/processed/data/recipes/IAdvancedLightningConcentratorRecipe.java
@@ -0,0 +1,28 @@
+package com.redcrafter07.processed.data.recipes;
+
+import com.redcrafter07.processed.Processed;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+
+public interface IAdvancedLightningConcentratorRecipe extends IRecipe<Inventory> {
+    //Credit: Kaupenjoe
+    ResourceLocation TYPE_ID = new ResourceLocation(Processed.MOD_ID, "advanced_lightning");
+
+    @Override
+    default IRecipeType<?> getType(){
+        return Registry.RECIPE_TYPE.getOptional(TYPE_ID).get();
+    }
+
+    @Override
+    default boolean canFit(int width, int height) {
+        return true;
+    }
+
+    @Override
+    default boolean isDynamic(){
+        return true;
+    }
+}
