af3dec4288f2b9cec24c1e89177045b0191fa8bd
RedCrafter07 <github@redcrafter07.de>
2022-04-12 11:45:37 +0200

    Make sword unenchantable and unrepairable

--- src/main/java/com/redcrafter07/processed/item/ProcessorSwordItem.java
+++ src/main/java/com/redcrafter07/processed/item/ProcessorSwordItem.java
@@ -1,12 +1,14 @@
 package com.redcrafter07.processed.item;
 
+import com.google.common.collect.Multimap;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
-
-import java.util.function.Consumer;
 
 public class ProcessorSwordItem extends SwordItem {
 
@@ -14,37 +16,18 @@ public class ProcessorSwordItem extends SwordItem {
         super(itemTier, attackDamage, attackSpeedIn, builderIn);
     }
 
-    /*@Override
-    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        if(stack.getDamage() == 1)  {
-            return false;
-        }
-        stack.damageItem(1, attacker, (entity) -> {
-        });
-      return true;
-    }*/
-
     @Override
-    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
-        /*if(stack.getDamage() == 1)  {
-            return 0;
-        }*/
-        return super.damageItem(stack, amount, entity, onBroken);
+    public boolean isEnchantable(ItemStack stack) {
+        return false;
     }
 
-
     @Override
-    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        stack.damageItem(1, attacker, (entity) -> {
-            entity.sendBreakAnimation(EquipmentSlotType.MAINHAND);
-        });
-        return true;
+    public boolean isRepairable(ItemStack stack) {
+        return false;
     }
 
-    /*public ActionResult<?> setAttackDamage(int attackDamage, PlayerEntity player)    {
-        ItemStack stack = player.getHeldItem(Hand.MAIN_HAND);
-        return  ActionResult.resultSuccess(stack);
-    }*/
-
-
+    @Override
+    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+        return false;
+    }
 }
