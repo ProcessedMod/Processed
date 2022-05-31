6471b62af4e2d853a994f83bb99045d17a145437
RedCrafter07 <github@redcrafter07.de>
2022-05-31 18:37:04 +0200

    Add Blade and (Carbon Reinforced) Saw Blade

---
+++ src/main/java/com/redcrafter07/processed/item/SawBladeItem.java
@@ -0,0 +1,40 @@
+package com.redcrafter07.processed.item;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+import java.util.Objects;
+
+public class SawBladeItem extends ModItem {
+    private float damageWhenHeld = 3;
+
+    public SawBladeItem(Properties properties, String itemID, float damage) {
+        super(properties, itemID);
+        damageWhenHeld = damage;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(isSelected) {
+            entityIn.performHurtAnimation();
+
+            stack.shrink(1);
+
+            entityIn.entityDropItem(new ItemStack(ModItems.SAW_BLADE.get(), 1), 3F);
+
+            entityIn.attackEntityFrom(new DamageSource("BLADE"), 3F);
+
+        }
+    }
+}
