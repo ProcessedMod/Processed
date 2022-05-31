5b3aca9ad8264713c7950d6ceec8284d66cac776
RedCrafter07 <github@redcrafter07.de>
2022-05-31 10:27:26 +0200

    VoxelShape and rotation

--- src/main/java/com/redcrafter07/processed/blocks/BlockForgeBlock.java
+++ src/main/java/com/redcrafter07/processed/blocks/BlockForgeBlock.java
@@ -6,6 +6,7 @@ import com.redcrafter07.processed.tileentity.BlockForgeTile;
 import com.redcrafter07.processed.tileentity.ModTileEntities;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,12 +14,18 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
@@ -27,15 +34,39 @@ import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.stream.Stream;
 
-public class BlockForgeBlock extends Block {
+public class BlockForgeBlock extends HorizontalBlock {
     public BlockForgeBlock(Properties properties) {
         super(properties);
     }
 
+    public static final VoxelShape SHAPE = Stream.of(
+            Block.makeCuboidShape(1, 8, 1, 15, 9, 15),
+            Block.makeCuboidShape(1, 0, 0, 15, 8, 16),
+            Block.makeCuboidShape(15, 0, 1, 16, 8, 15),
+            Block.makeCuboidShape(0, 0, 1, 1, 8, 15)
+            ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
+
     @Override
     public boolean hasTileEntity(BlockState state) {
         return true;
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(HORIZONTAL_FACING);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite());
     }
 
     @Override
