d9b39c7c56705a4a47476a4b729d8a65d2ab7b42
RedCrafter07 <gh@r07.dev>
2025-10-18 15:16:48 +0200

    refactor(lang): Sort language file and add missing tooltips
    Also changed the wrong tooltip id of CreativePowerSourceBlock

--- src/main/kotlin/redcrafter07/processed/block/CreativePowerSourceBlock.kt
+++ src/main/kotlin/redcrafter07/processed/block/CreativePowerSourceBlock.kt
@@ -10,7 +10,7 @@ import redcrafter07.processed.block.machine_abstractions.TieredProcessedBlock
 import redcrafter07.processed.block.tile_entities.CreativePowerSourceBlockEntity
 
 class CreativePowerSourceBlock(tier: ProcessedTier) :
-    TieredProcessedBlock(Properties.of(), "block.creative_power_source", tier, ::CreativePowerSourceBlockEntity) {
+    TieredProcessedBlock(Properties.of(), "block.processed.creative_power_source", tier, ::CreativePowerSourceBlockEntity) {
     override fun rotationType() = RotationType.NonRotatable
 
     override fun neighborChanged(
